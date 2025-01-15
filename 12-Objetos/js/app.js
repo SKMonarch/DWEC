c353ba70d998dbb5b6283145f8a43be322cd0c55
SKMonarch <christiansanlu@gmail.com>
2025-01-15 09:39:52 +0100

    Constructor Objetos y Metodos de Objetos

--- 12-Objetos/js/app.js
+++ 12-Objetos/js/app.js
@@ -241,7 +241,7 @@ console.log(Object.entries(fruta));
 
 for (let valor of Object.values(fruta)){
     if(typeof valor !== 'function'){
-        console.log(valor;)
+        console.log(valor);
     }
 }
 
@@ -251,3 +251,134 @@ for (let valor of Object.values(fruta)){
 //Convierte a una cadena de texto en formato JSON
 const cadenaObjeto = JSON.stringify(fruta);
 console.log(cadenaObjeto);
+
+
+
+
+//Función constructura de Objetos
+function Persona(nombre, ap1, ap2, fnac, ocup, ojps = "Marrones"){
+    this.nombre = nombre;
+    this.apellido1 = ap1;
+    this.apellido2 = ap2;
+    this.fnacimiento = new Date(fnac);
+    this.ocupacion = ocup;
+    //propiedades con valores por defecto
+    //this.ojos = "Marrones";
+    this.ojos = this.ojos;
+
+
+    //Métodos
+    this.nombreCompleto = function(){
+        return `Mi nombre es ${this.nombre} ${this.apellido1} ${this.apellido2}`;
+    };
+
+
+    this.cambiarOcupacion = function(nuevaOcupacion){
+        return this.ocupacion = nuevaOcupacion;
+    };
+
+
+    this.obetenerEdad = function(){
+        const anhoActual = new Date().getFullYear();
+        return  anhoActual - this.fnacimiento.getFullYear();
+    }
+
+
+    this.infoCompleta = function(){
+        return `${this.nombreCompleto()} y trabajo de ${this.ocupacion} y tengo ${ this.obetenerEdad()}`
+    }
+}
+
+
+
+//Intanciar objetos Persona
+const miPadre = new Persona("Federico","Romero","Blanco","1964-02-24","Carnicero");
+const miMadre = new Persona("Juana","Santo","Nieve","1967-03-02","Cocinera","Amarillo");
+
+
+//Visualizamos el objeto
+
+for ( let v of  Object.values(miPadre)){
+    if(typeof v !== 'function'){
+        console.log(v);
+    }
+}
+
+//Cambiar propiedad en los objetos instanciados
+miMadre.ojos = "Verdes";
+console.log(miMadre.ojos);
+
+
+//añadir propiedad adicional a los objetos instanciados
+//no va en el constructor!! solo está en el objeto al que se la añadimos
+miMadre.altura = 1.70;
+console.log(miMadre.altura);
+console.log(miPadre.altura); // undefined no existe la propiedad
+
+
+
+//Como añado propiedades o métodos en el constructor
+//Persona.altura = 1.70; no
+Persona.prototype.altura = 1.70;
+Persona.prototype.infoOcupacion = function(){
+        return `Trabajo de ${this.ocupacion}`;
+}
+
+const yo = new Persona("Felip3","Pérez","Negro","2003-03-14","Camionero");
+
+console.log(yo);
+
+
+//Métodos asocuados a los objetos
+console.log("================================");
+console.log("Object.create()");
+// Crear un objeto basado en miMadre como prototipo
+const hija = Object.create(miMadre);
+hija.nombre = "Ana";
+hija.apellido1 = "Santo";
+console.log(hija.nombreCompleto()); 
+console.log("================================");
+
+console.log("Object.assign()"); 
+// Copiar las propiedades de miMadre en un nuevo objeto
+const copiaMadre = Object.assign({}, miMadre);
+console.log(copiaMadre);
+// se podría hacer  l mismo con el operador de propagación
+const copiaMadre2 = { ...miMadre };
+console.log(copiaMadre2);
+console.log("================================");
+console.log("Object.freeze()");
+// Congelar miMadre para que no se puedan modificar sus propiedades
+Object.freeze(miMadre);
+miMadre.nombre = "María"; // No se aplicará
+console.log(miMadre.nombre); 
+console.log("================================");
+console.log("Object.seal()");
+// Sellar copiaMadre para evitar añadir o eliminar propiedades
+Object.seal(copiaMadre);
+copiaMadre.apellido1 = "Santos"; // Permitido
+delete copiaMadre.apellido2; // No permitido
+console.log(copiaMadre);
+console.log("================================");
+console.log("Object.isSealed()");
+// Verificar si copiaMadre está sellado
+console.log(Object.isSealed(copiaMadre)); 
+// true
+console.log("================================");
+console.log("Object.isFrozen()");
+// Verificar si miMadre está congelado
+console.log(Object.isFrozen(miMadre)); 
+// true
+console.log("================================");
+
+console.log("Object.hasOwnProperty()");
+// Verificar si una propiedad pertenece al objeto y no a la cadena prototípica
+console.log(miMadre.hasOwnProperty("nombre")); 
+// true
+console.log(hija.hasOwnProperty("nombreCompleto")); 
+// false (porque lo hereda de miMadre)
+// Alternativa con el operador `in`
+console.log("nombre" in miMadre); 
+// true
+console.log("nombreCompleto" in hija); 
+console.log("================================");

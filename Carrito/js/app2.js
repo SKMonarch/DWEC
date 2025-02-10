d06062297469d922597ec22ee0960f049f9303e7
SKMonarch <christiansanlu@gmail.com>
2025-02-10 16:08:08 +0100

    carrito

---
+++ Carrito/js/app2.js
@@ -0,0 +1,16 @@
+
+const carrito = document.querySelector('#carrito tbody');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#carrito');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const carritoIcono = document.querySelector('#acarrito');
+let articulosCarrito = [];
+
+
+
+console.log(carrito);
+console.log(listaCursos);
+console.log(carritoIcono);
+console.log(vaciarCarritoBtn);
+console.log(contenedorCarrito);
+console.log(articulosCarrito);
